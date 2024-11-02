50998904922eb2dc87eaf634d9b4bda3bdf58126
nishantchy <nishantchy1234@gmail.com>
2024-11-03 01:26:36 +0545

    added pages

--- README.md
+++ README.md
@@ -12,13 +12,8 @@ pnpm dev
 bun dev
 ```
 
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
-
-You can start editing the page by modifying `app/page.tsx`. The page auto-updates as you edit the file.
-
-## Learn More
-
-To learn more about Next.js, take a look at the following resources:
-
-- [Lama Dev Youtube Channel](https://youtube.com/lamadev)
-- [Next.js](https://nextjs.org/learn)
+<!-- 1:19:52s -->
+<!-- 1:43:08 -->
+<!-- 1:57:43 -->
+<!-- 2:33:10 -->
+<!-- 3:09:18 -->
--- src/app/(dashboard)/student/page.tsx
+++ src/app/(dashboard)/student/page.tsx
@@ -1,7 +1,26 @@
+import Announcements from "@/components/Announcements";
+import BigCalendar from "@/components/BigCalender";
+import EventCalender from "@/components/EventCalender";
 import React from "react";
+import "react-big-calendar/lib/css/react-big-calendar.css";
 
 const StudentPage = () => {
-  return <div>StudentPage</div>;
+  return (
+    <div className="p-4 flex gap-4 flex-col xl:flex-row">
+      {/* left */}
+      <div className="w-full xl:w-2/3">
+        <div className="h-full bg-white p-4 rounded-md">
+          <h1 className="text-xl font-semibold">Schedule (4A)</h1>
+          <BigCalendar />
+        </div>
+      </div>
+      {/* RIGHT */}
+      <div className="w-full xl:w-1/3 flex flex-col gap-8">
+        <EventCalender />
+        <Announcements />
+      </div>
+    </div>
+  );
 };
 
 export default StudentPage;
